5fe6df6c93113cc784da57ec2a9dc893247ddfb5
Ghassen <bennasrghassen1@gmail.com>
2026-04-03 10:37:12 +0100

    Update boycottnet.h

--- BoycottNet/boycottnet.h
+++ BoycottNet/boycottnet.h
@@ -6,100 +6,84 @@
 using namespace std;
 
 class Produit {
-
 private:
-string nom_;
-string codeBarre_;
-string collaborations_;
-
+    string nom_;
+    string codeBarre_;
+    string collaborations_;
 public:
-Produit(const string& nom,const string& codeBarre,const string& collaborations);
-
-void afficher();
-
-void setNom(const string& nom);
-const string& getNom() const;
-
-
-void setCodeBarre(const string& code);
-const string& getCodeBarre() const;
-
-
-void setCollaborations(const string& collaborations);
-const string& getCollaborations() const;
-};
-
-class ProduitsBoycottes{
-
-private:
-    vector<Produit> PBoycottes_;
-
-public:
-    void ajouter();
-    void supprimer();
+    Produit(const string& nom, const string& codeBarre, const string& collaborations);
 
     void afficher();
 
+    void setNom(const string& nom);
+    const string& getNom() const;
+
+    void setCodeBarre(const string& code);
+    const string& getCodeBarre() const;
+
+    void setCollaborations(const string& collaborations);
+    const string& getCollaborations() const;
+};
+
+class ProduitsBoycottes {
+private:
+    vector<Produit> PBoycottes_;
+public:
+    void ajouter();
+    void ajouter(const Produit& p);
+    void supprimer();
+    void afficher();
     void verif_code();
     void verif_nom();
 };
 
-class ProduitsAlternatifs{
-
+class ProduitsAlternatifs {
 private:
     vector<Produit> PAlternatifs_;
-
 public:
     void ajouter();
+    void ajouter(const Produit& p);
     void supprimer();
-
     void afficher();
 };
 
 class Site {
-
 private:
-string nom_;
-string url_;
-string infos_;
-
+    string nom_;
+    string url_;
+    string infos_;
 public:
-Site(const string& nom, const string& url,const string& infos);
+    Site(const string& nom, const string& url, const string& infos);
 
-void setNom(const string& nom);
-const string& getNom() const;
+    void setNom(const string& nom);
+    const string& getNom() const;
 
-void setURL(const string& url);
-const string& getURL() const;
+    void setUrl(const string& url);
+    const string& getUrl() const;
 
-void setInfos(const string& infos);
-const string& getInfos() const;
-void afficher();
+    void setInfos(const string& infos);
+    const string& getInfos() const;
+    void afficher();
 };
 
-class SitesBoycotts{
-
+class SitesBoycotts {
 private:
     vector<Site> SBoycottes_;
-
 public:
     void ajouter();
+    void ajouter(const Site& s);
     void supprimer();
-
     void afficher();
-
     void verif_url();
     void verif_nom();
 };
 
-class SitesAlternatifs{
-
+class SitesAlternatifs {
 private:
     vector<Site> SAlternatifs_;
-
 public:
     void ajouter();
+    void ajouter(const Site& s);
     void supprimer();
-
     void afficher();
 };
